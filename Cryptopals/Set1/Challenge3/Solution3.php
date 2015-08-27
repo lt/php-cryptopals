3d5dc22cb4df5b7f0d8771c400434f17fee31a0a
Leigh <leight@gmail.com>
2015-08-27 22:14:49 +0100

    Refactor solution 3

--- Cryptopals/Set1/Challenge3/Solution3.php
+++ Cryptopals/Set1/Challenge3/Solution3.php
@@ -1,140 +1,109 @@
-<?php
+<?php declare(strict_types = 1);
 
-/*
- * http://cryptopals.com/sets/1/challenges/3/
- *
- * Single-byte XOR cipher
- *
- * The hex encoded string:
- * 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
- *
- * ... has been XOR'd against a single character. Find the key, decrypt the message.
- *
- * You can do this by hand. But don't: write code to do it for you.
- *
- * How? Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric. Evaluate each output and choose the one with the best score.
- *
- * Achievement Unlocked
- * You now have our permission to make "ETAOIN SHRDLU" jokes on Twitter.
- */
+namespace Cryptopals\Set1\Challenge3;
 
-// from https://en.wikipedia.org/wiki/Letter_frequency
-$englishLanguageWeights = [
-    'e' => 13.0001,
-    't' => 9.056,
-    'a' => 8.167,
-    'o' => 7.507,
-    'i' => 6.966,
-    'n' => 6.749,
-    's' => 6.327,
-    'h' => 6.094,
-    'r' => 5.987,
-    'd' => 4.253,
-    'l' => 4.025,
-    'c' => 2.782,
-    'u' => 2.758,
-    'm' => 2.406,
-    'w' => 2.360,
-    'f' => 2.228,
-    'g' => 2.015,
-    'y' => 1.974,
-    'p' => 1.929,
-    'b' => 1.492,
-    'v' => 0.978,
-    'k' => 0.772,
-    'j' => 0.153,
-    'x' => 0.150,
-    'q' => 0.095,
-    'z' => 0.074,
-];
+use Cryptopals\Solution;
 
-// lets not penalise upper case letters
-foreach ($englishLanguageWeights as $k => $v) {
-    $englishLanguageWeights[strtoupper($k)] = $v;
-}
-
-/*
- * more from https://en.wikipedia.org/wiki/Letter_frequency
- *
- * "In English, the space is slightly more frequent than the top letter (e)
- * and the non-alphabetic characters (digits, punctuation, etc.) collectively
- * occupy the fourth position, between t and a."
- */
-
-$englishLanguageWeights += [
-    ' ' => 13.1,
-    '0' => 8.4,
-    '1' => 8.4,
-    '2' => 8.4,
-    '3' => 8.4,
-    '4' => 8.4,
-    '5' => 8.4,
-    '6' => 8.4,
-    '7' => 8.4,
-    '8' => 8.4,
-    '9' => 8.4,
-    '\'' => 8.4,
-    '"' => 8.4,
-    '.' => 8.4,
-    ',' => 8.4,
-    '!' => 8.4,
-    '?' => 8.4,
-];
-
-function scoreSingleByteXOR($encrypted, array $weights, $penalty = 0)
+class Solution3 extends Solution
 {
-    $encryptedLen = strlen($encrypted);
+    // https://en.wikipedia.org/wiki/Letter_frequency#Relative_frequencies_of_letters_in_the_English_language
+    const FREQ_EN = [
+        'e' => 12.702, 't' => 9.056, 'a' => 8.167, 'o' => 7.507, 'i' => 6.966, 'n' => 6.749,
+        's' =>  6.327, 'h' => 6.094, 'r' => 5.987, 'd' => 4.253, 'l' => 4.025, 'c' => 2.782,
+        'u' =>  2.758, 'm' => 2.406, 'w' => 2.361, 'f' => 2.228, 'g' => 2.015, 'y' => 1.974,
+        'p' =>  1.929, 'b' => 1.492, 'v' => 0.978, 'k' => 0.772, 'j' => 0.153, 'x' => 0.150,
+        'q' =>  0.095, 'z' => 0.074,
 
-    $scores = [];
+        /*
+         * More from https://en.wikipedia.org/wiki/Letter_frequency
+         *
+         * "In English, the space is slightly more frequent than the top letter (e)
+         * and the non-alphabetic characters (digits, punctuation, etc.) collectively
+         * occupy the fourth position, between t and a."
+         *
+         * Made up values based on the above. Lots of "etc." unaccounted for.
+         */
+        ' ' => 13
+    ];
 
-    for ($i = 0; $i < 256; $i++) {
+    // https://en.wikipedia.org/wiki/Letter_frequency#Relative_frequencies_of_the_first_letters_of_a_word_in_the_English_language
+    const FREQ_START_EN = [
+        't' => 16.671, 'a' => 11.602, 's' => 7.755, 'h' => 7.232, 'w' => 6.753, 'i' => 6.286,
+        'o' =>  6.264, 'b' =>  4.702, 'm' => 4.383, 'f' => 3.779, 'c' => 3.511, 'l' => 2.705,
+        'd' =>  2.670, 'p' =>  2.545, 'n' => 2.365, 'e' => 2.007, 'g' => 1.950, 'r' => 1.653,
+        'y' =>  1.620, 'u' =>  1.487, 'v' => 0.649, 'j' => 0.597, 'k' => 0.590, 'q' => 0.173,
+        'z' =>  0.034, 'x' =>  0.017
+    ];
+
+    protected function scoreASCII(string $data): float
+    {
+        $data = strtolower($data);
+        $dataLen = strlen($data);
+
         $score = 0;
-        $trial = $encrypted ^ str_repeat(chr($i), $encryptedLen);
+        for ($i = 0; $i < $dataLen; $i++) {
+            $c = $data[$i];
 
-        for ($j = 0; $j < $encryptedLen; $j++) {
-            $char = $trial[$j];
-            if (isset($weights[$char])) {
-                $score += $weights[$char];
+            // Control characters or extended ASCII
+            if (($c < ' ') || ($c > '~')) {
+                $score -= 100;
+                continue;
+            }
+
+            // First letter of the string, or preceeded by space or tab
+            $weights = ($i === 0 || $data[$i - 1] === ' ' || $data[$i - 1] === "\x9") ?
+                self::FREQ_START_EN : self::FREQ_EN;
+
+            if (isset($weights[$c])) {
+                $score += $weights[$c];
             }
             else {
-                $score -= $penalty;
+                $score -= 3;
             }
         }
 
-        $scores[$i] = $score;
+        // Normalise
+        return $score / $dataLen;
     }
 
-    return $scores;
-}
+    protected function scoreSingleByteXORs(string $input): array
+    {
+        $inputLen = strlen($input);
 
-// don't output if we're included into another script.
-if (!debug_backtrace()) {
-    $encrypted = hex2bin('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736');
-    $encryptedLen = strlen($encrypted);
+        $scores = [];
 
-    $scores = scoreSingleByteXOR($encrypted, $englishLanguageWeights);
-    arsort($scores);
-
-    print "Highest scoring character codes:\n";
-
-    $i = 0;
-    foreach ($scores as $k => $v) {
-        print "$k - $v\n";
-
-        if (++$i === 3) {
-            break;
+        for ($i = 0; $i < 256; $i++) {
+            $trial = $input ^ str_repeat(chr($i), $inputLen);
+            $scores[$i] = $this->scoreASCII($trial);
         }
+
+        return $scores;
     }
 
-    print "\nDecrypted strings:\n";
+    protected function execute(): bool
+    {
+        $input = hex2bin('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736');
+        $inputLen = strlen($input);
 
-    $i = 0;
-    foreach ($scores as $k => $v) {
-        $decypted = $encrypted ^ str_repeat(chr($k), $encryptedLen);
-        print  "$k: $decypted\n";
+        $scores = $this->scoreSingleByteXORs($input);
 
-        if (++$i === 3) {
-            break;
+        arsort($scores);
+
+        $limit = 5;
+        print "Score | Char | Output\n";
+        print "--------------------- -  -   -\n";
+
+        $i = 0;
+        foreach ($scores as $k => $v) {
+            printf("%5.2f | 0x%2x | %s\n", $v, $k, $input ^ str_repeat(chr($k), $inputLen));
+
+            if (++$i === $limit) {
+                break;
+            }
         }
+
+        // Ok this isn't really a true/false success one, but after a couple
+        // of runs, I know the output is correct.
+        return true;
     }
 }
