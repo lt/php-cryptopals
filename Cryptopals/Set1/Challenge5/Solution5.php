9304a9f1d3394b301d924cd4f1938d39cfc3a5a1
Leigh <leight@gmail.com>
2015-08-28 19:16:28 +0100

    Refactor solution 5

--- Cryptopals/Set1/Challenge5/Solution5.php
+++ Cryptopals/Set1/Challenge5/Solution5.php
@@ -1,53 +1,35 @@
-<?php
+<?php declare(strict_types = 1);
 
-/*
- * http://cryptopals.com/sets/1/challenges/5/
- *
- * Implement repeating-key XOR
- *
- * Here is the opening stanza of an important work of the English language:
- * Burning 'em, if you ain't quick and nimble
- * I go crazy when I hear a cymbal
- *
- * Encrypt it, under the key "ICE", using repeating-key XOR.
- *
- * In repeating-key XOR, you'll sequentially apply each byte of the key; the first byte of plaintext will be XOR'd against I, the next C, the next E, then I again for the 4th byte, and so on.
- *
- * It should come out to:
- * 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
- *
- * Encrypt a bunch of stuff using your repeating-key XOR function. Encrypt your mail. Encrypt your password file. Your .sig file. Get a feel for it. I promise, we aren't wasting your time with this.
- */
+namespace Cryptopals\Set1\Challenge5;
 
-function repeatingKeyXOR($data, $key)
+use Cryptopals\Solution;
+
+class Solution5 extends Solution
 {
-    $dataLen = strlen($data);
-    $keyLen = strlen($key);
+    protected function repeatingKeyXOR(string $input, string $key): string
+    {
+        $inputLen = strlen($input);
+        $keyLen = strlen($key);
 
-    for ($i = 0; $i < $dataLen; $i++) {
-        $data[$i] = $data[$i] ^ $key[$i % $keyLen];
+        for ($i = 0; $i < $inputLen; $i++) {
+            $input[$i] = $input[$i] ^ $key[$i % $keyLen];
+        }
+
+        return $input;
     }
 
-    return $data;
-}
+    protected function execute(): bool
+    {
+        $input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        $key = 'ICE';
+        $expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f';
 
-// don't output if we're included into another script.
-if (!debug_backtrace()) {
-    $data = <<<EOF
-Burning 'em, if you ain't quick and nimble
-I go crazy when I hear a cymbal
-EOF;
+        $output = $this->repeatingKeyXOR($input, $key);
+        $output = bin2hex($output);
 
-    $key = 'ICE';
+        print 'Expected: ' . $expected . "\n";
+        print 'Actual:   ' . $output . "\n";
 
-    $expected = hex2bin('0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f');
-
-    print "Sanity checking using one-time pad\n";
-    $sanity = ($data ^ str_repeat($key, ceil(strlen($data) / strlen($key)))) === $expected;
-    print $sanity ? "Success!\n\n" : "Failure :(\n\n";
-
-    print "Performing repeating key XOR with homebrew function\n";
-    $homebrew = repeatingKeyXOR($data, $key);
-    $homebrewSane = $homebrew === $expected;
-    print $homebrewSane ? "Success!\n\n" : "Failure :(\n\n";
+        return $output === $expected;
+    }
 }
