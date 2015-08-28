de718c6d5a41c244554de8bab4480abcd741ffc8
Leigh <leight@gmail.com>
2015-08-28 19:25:52 +0100

    Refactor solution 6

--- Cryptopals/Set1/Challenge6/Solution6.php
+++ Cryptopals/Set1/Challenge6/Solution6.php
@@ -1,157 +1,89 @@
-<?php
+<?php declare(strict_types = 1);
 
-/*
- * http://cryptopals.com/sets/1/challenges/6/
- *
- * Break repeating-key XOR
- *
- * It is officially on, now.
- * This challenge isn't conceptually hard, but it involves actual error-prone coding. The other challenges in this set are there to bring you up to speed. This one is there to qualify you. If you can do this one, you're probably just fine up to Set 6.
- *
- * There's a file here. It's been base64'd after being encrypted with repeating-key XOR.
- *
- * Decrypt it.
- *
- * Here's how:
- *
- * 1. Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
- *
- * 2. Write a function to compute the edit distance/Hamming distance between two strings. The Hamming distance is just the number of differing bits. The distance between:
- * this is a test
- * and
- * wokka wokka!!!
- * is 37. Make sure your code agrees before you proceed.
- *
- * 3. For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
- *
- * 4. The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
- *
- * 5. Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
- *
- * 6. Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
- *
- * 7. Solve each block as if it was single-character XOR. You already have code to do this.
- *
- * 8. For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
- *
- * This code is going to turn out to be surprisingly useful later on. Breaking repeating-key XOR ("Vigenere") statistically is obviously an academic exercise, a "Crypto 101" thing. But more people "know how" to break it than can actually break it, and a similar technique breaks something much more important.
- *
- * No, that's not a mistake.
- * We get more tech support questions for this challenge than any of the other ones. We promise, there aren't any blatant errors in this text. In particular: the "wokka wokka!!!" edit distance really is 37.
- */
+namespace Cryptopals\Set1\Challenge6;
 
-require_once '04-detect-single-character-xor.php';
+use Cryptopals\Set1\Challenge4\Solution4;
 
-// popcount the diff (xor) bits between two strings
-function hammingDistance($one, $two)
+class Solution6 extends Solution4
 {
-    // easier to popcount strings if we convert them into ints
-    $diffs = unpack('S*', $one ^ $two);
-    $count = 0;
+    // popcount the diff (xor) bits between two strings
+    protected function hammingDistance($one, $two)
+    {
+        $count = 0;
 
-    foreach ($diffs as $diff) {
-        while ($diff) {
-            $diff &= $diff - 1;
-            $count++;
-        }
-    }
-
-    return $count;
-}
-
-function scoreHammedKeySizeRange($data, $lowKeySize, $highKeySize, $sampleLimit = 0)
-{
-    $dataLen = strlen($data);
-    $scores = [];
-
-    for($keyLen = $lowKeySize; $keyLen <= $highKeySize; $keyLen++) {
-        $maxBlocks = (int)($dataLen / $keyLen);
-
-        $samples = 0;
-        $score = 0;
-        for ($sampleOneIndex = 0; $sampleOneIndex < $maxBlocks; $sampleOneIndex++) {
-            for ($sampleTwoIndex = 0; $sampleTwoIndex < $maxBlocks; $sampleTwoIndex++) {
-                if ($sampleOneIndex !== $sampleTwoIndex) {
-                    $one = substr($data, $sampleOneIndex * $keyLen, $keyLen);
-                    $two = substr($data, $sampleTwoIndex * $keyLen, $keyLen);
-                    $score += hammingDistance($one, $two);
-                    $samples++;
-                    if ($sampleLimit && $samples === $sampleLimit) {
-                        break 2;
-                    }
-                }
+        foreach (unpack('C*', $one ^ $two) as $diff) {
+            while ($diff) {
+                $diff &= $diff - 1;
+                $count++;
             }
         }
-        $scores[$keyLen] = $score / $keyLen / $samples;
+
+        return $count;
     }
 
-    asort($scores);
-    return $scores;
-}
+    protected function scoreKeyLengths($data, $lowKeySize, $highKeySize)
+    {
+        $scores = [];
 
-function transposeBlocks(array $blocks)
-{
-    $blockLens = array_map('strlen', $blocks);
-    $blocks = array_map('str_split', $blocks);
-    $iterations = max($blockLens);
-    $newBlocks = [];
+        for($keyLen = $lowKeySize; $keyLen <= $highKeySize; $keyLen++) {
+            $samples = 0;
+            $summedDistance = 0;
 
-    for ($i = 0; $i < $iterations; $i++) {
-        $newBlocks[] = array_column($blocks, $i);
-    }
+            $chunks = str_split($data, $keyLen);
+            $chunkCount = count($chunks);
+            for ($a = 0; $a < $chunkCount; $a++) {
+                for ($b = $a + 1; $b < $chunkCount; $b++) {
+                    $summedDistance += $this->hammingDistance($chunks[$a], $chunks[$b]);
+                    $samples++;
+                }
+            }
 
-    return array_map('implode', $newBlocks);
-}
-
-// don't output if we're included into another script.
-if (!debug_backtrace()) {
-    $data = base64_decode(file_get_contents('06-data.txt'));
-    $dataLen = strlen($data);
-
-    print "Sanity check hamming function\n";
-    $sanity = hammingDistance('this is a test', 'wokka wokka!!!') === 37;
-    print $sanity ? "Success!\n\n" : "Failure :(\n\n";
-
-    print "This will take a while. Hold on!\n\n";
-    $scores = scoreHammedKeySizeRange($data, 2, 40, 10000); // 10k sample limit to reduce time taken
-
-    print "Top scoring key sizes:\n";
-    $i = 0;
-    foreach ($scores as $k => $v) {
-        print "$k - $v\n";
-
-        if (++$i === 3) {
-            break;
+            // Average distance per character across all samples
+            $scores[$keyLen] = $summedDistance / $keyLen / $samples;
         }
+
+        return $scores;
     }
 
-    $potentialKeys = [];
+    protected function transposeBlocks(array $blocks)
+    {
+        $matrix = array_map('str_split', $blocks);
+        $iterations = count($matrix[0]);
+        $newBlocks = [];
 
-    print "\nSolving keys based on English Language scoring:\n";
-    $i = 0;
-    foreach ($scores as $k => $v) {
-        $blocks = str_split($data, $k);
-        $blocks = transposeBlocks($blocks);
-
-        list($topScores, $topChars) = scoreSingleByteXORStrings($blocks, $englishLanguageWeights);
-
-        $potentialKeys[$k] = implode(array_map('chr', $topChars));
-        print "$k: {$potentialKeys[$k]}\n";
-
-        if (++$i === 3) {
-            break;
+        for ($i = 0; $i < $iterations; $i++) {
+            $newBlocks[] = array_column($matrix, $i);
         }
+
+        return array_map('implode', $newBlocks);
     }
 
-    print "\nDecrypted data with solved keys:\n";
-    $i = 0;
-    foreach ($scores as $k => $v) {
-        $decrypted = $data ^ str_repeat($potentialKeys[$k], ceil($dataLen / strlen($potentialKeys[$k])));
+    protected function execute(): bool
+    {
+        $data = base64_decode(file_get_contents(__DIR__ . '/6.txt'));
 
-        print "$k: $decrypted\n\n\n";
+        $scores = $this->scoreKeyLengths($data, 2, 40);
+        asort($scores);
 
-        if (++$i === 3) {
-            break;
+        $limit = 5;
+        print "Size | Score | Key\n";
+        print "------------------ -  -   -\n";
+
+        $i = 0;
+        foreach ($scores as $k => $v) {
+            $blocks = str_split($data, $k);
+            $blocks = $this->transposeBlocks($blocks);
+
+            list($topScores, $topChars) = $this->scoreSingleByteXORStrings($blocks);
+            printf("%4u | %5.3f | %s\n", $k, $v, implode(array_map('chr', $topChars)));
+
+            if (++$i === $limit) {
+                break;
+            }
         }
+
+        // Ok this isn't really a true/false success one, but after a couple
+        // of runs, I know the output is correct.
+        return true;
     }
 }
