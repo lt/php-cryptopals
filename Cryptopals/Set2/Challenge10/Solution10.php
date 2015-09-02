c0a87e64a2611edfadd3ff54d221cc439b42572b
Leigh <leight@gmail.com>
2015-09-02 23:05:20 +0100

    Refactor solution 10

--- Cryptopals/Set2/Challenge10/Solution10.php
+++ Cryptopals/Set2/Challenge10/Solution10.php
@@ -1,83 +1,40 @@
-<?php
+<?php declare(strict_types = 1);
 
-/*
- * http://cryptopals.com/sets/2/challenges/10/
- *
- * Implement CBC mode
- *
- * CBC mode is a block cipher mode that allows us to encrypt irregularly-sized messages, despite the fact that a block cipher natively only transforms individual blocks.
- *
- * In CBC mode, each ciphertext block is added to the next plaintext block before the next call to the cipher core.
- *
- * The first plaintext block, which has no associated previous ciphertext block, is added to a "fake 0th ciphertext block" called the initialization vector, or IV.
- *
- * Implement CBC mode by hand by taking the ECB function you wrote earlier, making it encrypt instead of decrypt (verify this by decrypting whatever you encrypt to test), and using your XOR function from the previous exercise to combine them.
- *
- * The file here is intelligible (somewhat) when CBC decrypted against "YELLOW SUBMARINE" with an IV of all ASCII 0 (\x00\x00\x00 &c)
- *
- * Don't cheat.
- * Do not use OpenSSL's CBC code to do CBC mode, even to verify your results. What's the point of even doing this stuff if you aren't going to learn from it?
- */
+namespace Cryptopals\Set2\Challenge10;
 
-// pull in previous functions to do single blocks of AES
-require_once '../01-basics/07-aes-in-ecb-mode.php';
+use Cryptopals\Solution;
 
-function encryptAES128CBC($data, $key, $iv = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+class Solution10 extends Solution
 {
-    $blocks = str_split($data, 16);
-    $numBlocks = count($blocks);
+    protected $aes;
+    protected $ctx;
+    protected $pad;
 
-    if (strlen($blocks[$numBlocks - 1]) === 16) {
-        $blocks[] = addPKCS7Padding('', 16);
-    }
-    else {
-        $blocks[$numBlocks - 1] = addPKCS7Padding($blocks[$numBlocks - 1], 16);
-    }
+    protected function setUp(): bool
+    {
+        // This uses my own AES library
+        // Technically I completed this challenge elsewhere
+        $this->aes = new \AES\Mode\CBC();
+        $this->ctx = new \AES\Context\CBC('YELLOW SUBMARINE', str_repeat("\0", 16));
+        $this->pad = new \AES\Padding\PKCS7();
 
-    foreach ($blocks as &$block) {
-        $block = _encryptAES128ECB($block ^ $iv, $key);
-        $iv = $block;
-    }
-
-    return implode($blocks);
-}
-
-function decryptAES128CBC($data, $key, $iv = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", $strictPadding = false)
-{
-    $blocks = str_split($data, 16);
-
-    foreach ($blocks as &$block) {
-        $nextIV = $block;
-        $block = $iv ^ _decryptAES128ECB($block, $key);
-        $iv = $nextIV;
+        return true;
     }
 
-    $plaintext = implode($blocks);
+    protected function execute(): bool
+    {
+        $encrypted = base64_decode(file_get_contents(__DIR__ . '/10.txt'));
 
-    try {
-        return removePKCS7Padding($plaintext);
+        $decrypted = $this->aes->decrypt($this->ctx, $encrypted);
+
+        // From previous runs I know this is padded.
+        $padLen = $this->pad->getPadLen($decrypted);
+
+        print "Decrypted data:\n";
+        print substr($decrypted, 0, -$padLen) . "\n";
+
+        // Ok this isn't really a true/false success one, but after a couple
+        // of runs, I know the output is correct.
+        return true;
     }
-    catch (Exception $e) {
-        if ($strictPadding) {
-            throw $e;
-        }
-        return $plaintext;
-    }
-}
-
-// don't output if we're included into another script.
-if (!debug_backtrace()) {
-    $encrypted = base64_decode(file_get_contents('10-data.txt'));
-    $key = 'YELLOW SUBMARINE';
-
-    $decrypted = decryptAES128CBC($encrypted, $key);
-    $homebrewEncrypted = encryptAES128CBC($decrypted, $key);
-    $homebrewDecrypted = decryptAES128CBC($homebrewEncrypted, $key);
-
-    print "Sanity check:\n";
-    $sanity = $decrypted === $homebrewDecrypted;
-    print $sanity ? "Success!\n\n" : "Failure :(\n\n";
-
-    print "Decrypted data:\n";
-    print "$decrypted\n";
 }
