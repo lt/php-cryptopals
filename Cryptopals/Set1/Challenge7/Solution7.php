cbab2c82b9095f5b6a0a125a58382a30fed45f15
Leigh <leight@gmail.com>
2015-09-02 20:13:13 +0100

    Use my own AES lib

--- Cryptopals/Set1/Challenge7/Solution7.php
+++ Cryptopals/Set1/Challenge7/Solution7.php
@@ -2,48 +2,21 @@
 
 namespace Cryptopals\Set1\Challenge7;
 
-use Cryptopals\Common\AES128;
 use Cryptopals\Solution;
 
 class Solution7 extends Solution
 {
     protected $aes;
-    protected $encryptionKey;
-    protected $decryptionKey;
-
-    protected function encrypt(string $message): string
-    {
-        $offset = 0;
-        $out = '';
-        $messageLen = strlen($message);
-        $blocks = $messageLen >> 4;
-
-        while ($blocks--) {
-            $out .= $this->aes->encryptBlock($this->encryptionKey, substr($message, $offset, 16));
-            $offset += 16;
-        }
-
-        return $out;
-    }
-
-    protected function decrypt(string $message)
-    {
-        $offset = 0;
-        $out = '';
-        $messageLen = strlen($message);
-        $blocks = $messageLen >> 4;
-
-        while ($blocks--) {
-            $out .= $this->aes->decryptBlock($this->decryptionKey, substr($message, $offset, 16));
-            $offset += 16;
-        }
-
-        return $out;
-    }
+    protected $ctx;
+    protected $pad;
 
     protected function setUp(): bool
     {
-        $this->aes = new AES128();
+        // This uses my own AES library
+        // Technically I completed this challenge elsewhere
+        $this->aes = new \AES\Mode\ECB();
+        $this->ctx = new \AES\Context\ECB('YELLOW SUBMARINE');
+        $this->pad = new \AES\Padding\PKCS7();
 
         return true;
     }
@@ -51,12 +24,14 @@ class Solution7 extends Solution
     protected function execute(): bool
     {
         $encrypted = base64_decode(file_get_contents(__DIR__ . '/7.txt'));
-        list($this->encryptionKey, $this->decryptionKey) = $this->aes->init('YELLOW SUBMARINE');
 
-        $decrypted = $this->decrypt($encrypted);
+        $decrypted = $this->aes->decrypt($this->ctx, $encrypted);
+
+        // From previous runs I know this is padded.
+        $padLen = $this->pad->getPadLen($decrypted);
 
         print "Decrypted data:\n";
-        print $decrypted . "\n";
+        print substr($decrypted, 0, -$padLen) . "\n";
 
         // Ok this isn't really a true/false success one, but after a couple
         // of runs, I know the output is correct.
