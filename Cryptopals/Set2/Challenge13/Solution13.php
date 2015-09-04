04abe923c9e14f11540b5ed2d8d04b6f89e84066
Leigh <leight@gmail.com>
2015-09-04 19:28:16 +0100

    Refactor solution 13

--- Cryptopals/Set2/Challenge13/Solution13.php
+++ Cryptopals/Set2/Challenge13/Solution13.php
@@ -1,104 +1,91 @@
-<?php
+<?php declare(strict_types = 1);
 
-/*
- * http://cryptopals.com/sets/2/challenges/13/
- *
- * ECB cut-and-paste
- *
- * Write a k=v parsing routine, as if for a structured cookie. The routine should take:
- * foo=bar&baz=qux&zap=zazzle
- *
- * ... and produce:
- * {
- *   foo: 'bar',
- *   baz: 'qux',
- *   zap: 'zazzle'
- * }
- *
- * (you know, the object; I don't care if you convert it to JSON).
- *
- * Now write a function that encodes a user profile in that format, given an email address. You should have something like:
- * profile_for("foo@bar.com")
- *
- * ... and it should produce:
- * {
- *   email: 'foo@bar.com',
- *   uid: 10,
- *   role: 'user'
- * }
- *
- * ... encoded as:
- * email=foo@bar.com&uid=10&role=user
- *
- * Your "profile_for" function should not allow encoding metacharacters (& and =). Eat them, quote them, whatever you want to do, but don't let people set their email address to "foo@bar.com&role=admin".
- *
- * Now, two more easy functions. Generate a random AES key, then:
- * A. Encrypt the encoded user profile under the key; "provide" that to the "attacker".
- * B. Decrypt the encoded user profile and parse it.
- *
- * Using only the user input to profile_for() (as an oracle to generate "valid" ciphertexts) and the ciphertexts themselves, make a role=admin profile.
- */
+namespace Cryptopals\Set2\Challenge13;
 
-require_once '../utils/random-bytes.php';
-require_once '../01-basics/07-aes-in-ecb-mode.php';
+use Cryptopals\Set1\Challenge8\Solution8;
 
-function profileFor($email)
+class Solution13 extends Solution8
 {
-    return http_build_query([
-        'email' => $email,
-        'uid' => 10,
-        'role' => 'user'
-    ]);
-}
+    protected $ecb;
+    protected $ctx;
+    protected $pad;
 
-function encryptedProfileFor($email, $key)
-{
-    return encryptAES128ECB(profileFor($email), $key);
-}
+    protected function setUp(): bool
+    {
+        $key = random_bytes(16);
 
-function decryptedProfile($ciphertext, $key)
-{
-    parse_str(decryptAES128ECB($ciphertext, $key), $profile);
-    return $profile;
-}
+        $this->ecb = new \AES\Mode\ECB();
+        $this->ctx = new \AES\Context\ECB($key);
+        $this->pad = new \AES\Padding\PKCS7();
 
-$key = getRandomBytes(16);
+        return true;
+    }
 
-// assuming we know the structure is email=&uid=&role=
-// 0..............f|0............
-// email=aaaaaaaaaa|admin&uid=...
+    protected function profileFor(string $email): string
+    {
+        return http_build_query([
+            'email' => $email,
+            'uid' => 10,
+            'role' => 'user'
+        ]);
+    }
 
-$padToAlignEmail = 16 - strlen('email=');
+    protected function encryptedProfileFor(string $email): string
+    {
+        $profile = $this->profileFor($email);
 
-// pad until we cause a block count increase, then add 3 so we can chop off "user"
-// ....f|0......
-// role=|user...
+        return $this->ecb->encrypt($this->ctx, $profile . $this->pad->getPadding($profile));
+    }
 
-$lastLen = strlen(encryptedProfileFor('a', $key));
-for ($i = 2; $i <= 16; $i++) {
-    if ($lastLen !== strlen(encryptedProfileFor(str_repeat('a', $i), $key))) {
-        break;
+    protected function decryptedProfile(string $ciphertext): array
+    {
+        $decypted = $this->ecb->decrypt($this->ctx, $ciphertext);
+
+        parse_str($decypted, $profile);
+        return $profile;
+    }
+
+    protected function execute(): bool
+    {
+        // assuming we know the structure is email=&uid=&role=
+        // 0..............f|0............
+        // email=aaaaaaaaaa|admin&uid=...
+
+        $padToAlignEmail = 16 - strlen('email=');
+
+        // pad until we cause a block count increase, then add 3 so we can chop off "user"
+        // ....f|0......
+        // role=|user...
+
+        $lastLen = strlen($this->encryptedProfileFor('a'));
+        for ($i = 2; $i <= 16; $i++) {
+            if ($lastLen !== strlen($this->encryptedProfileFor(str_repeat('a', $i)))) {
+                break;
+            }
+        }
+        $padToChopRole = $i + 4;
+
+        print "Padding to chop role: $padToChopRole\n";
+        print "Padding to align admin: $padToAlignEmail\n";
+
+        $adminInBlock2 = $this->encryptedProfileFor(str_repeat('a', $padToAlignEmail) . 'admin');
+        $roleInBlock3 = $this->encryptedProfileFor(str_repeat('a', $padToChopRole));
+
+        // cut and paste
+        // 0..............f|0..............f|0..............f
+        // <------------- this ------------>|
+        // email=aaaaaaaaaa|aaa&uid=10&role=|user
+        //                 |                |<-- and this -->
+        //                 |email=aaaaaaaaaa|admin&uid=10&rol
+        //                 |                |
+        // email=aaaaaaaaaa|aaa&uid=10&role=|admin&uid=10&rol
+        
+        $bakedAuth = substr($roleInBlock3, 0, 32) . substr($adminInBlock2, 16, 16);
+        $decryptedProfile = $this->decryptedProfile($bakedAuth);
+
+        print "Decrypted profile:\n";
+        var_dump($decryptedProfile);
+
+        return true;
     }
 }
-$padToChopRole = $i + 4;
-
-print "Padding to chop role: $padToChopRole\n";
-print "Padding to align admin: $padToAlignEmail\n";
-
-$adminInBlock2 = encryptedProfileFor(str_repeat('a', $padToAlignEmail) . 'admin', $key);
-$roleInBlock3 = encryptedProfileFor(str_repeat('a', $padToChopRole), $key);
-
-// cut and paste
-// 0..............f|0..............f|0..............f
-// <------------- this ------------>|
-// email=aaaaaaaaaa|aaa&uid=10&role=|user
-//                 |                |<-- and this -->
-//                 |email=aaaaaaaaaa|admin&uid=10&rol
-//                 |                |
-// email=aaaaaaaaaa|aaa&uid=10&role=|admin&uid=10&rol
-
-$bakedAuth = substr($roleInBlock3, 0, 32) . substr($adminInBlock2, 16, 16);
-$decryptedProfile = decryptedProfile($bakedAuth, $key);
-
-print "Decrypted profile:\n";
-var_dump($decryptedProfile);
