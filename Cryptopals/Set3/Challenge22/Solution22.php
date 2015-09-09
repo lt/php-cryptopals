4a3ce4ae47f68bbc81d5d6cb33ca71171af37e50
Leigh <leight@gmail.com>
2015-09-09 19:48:28 +0100

    Refactor solution 22

--- Cryptopals/Set3/Challenge22/Solution22.php
+++ Cryptopals/Set3/Challenge22/Solution22.php
@@ -1,39 +1,31 @@
-<?php
+<?php declare(strict_types = 1);
 
-/*
- * http://cryptopals.com/sets/3/challenges/22/
- *
- * Crack an MT19937 seed
- *
- * Make sure your MT19937 accepts an integer seed value. Test it (verify that you're getting the same sequence of outputs given a seed).
- *
- * Write a routine that performs the following operation:
- * - Wait a random number of seconds between, I don't know, 40 and 1000.
- * - Seeds the RNG with the current Unix timestamp
- * - Waits a random number of seconds again.
- * - Returns the first 32 bit output of the RNG.
- *
- * You get the idea. Go get coffee while it runs. Or just simulate the passage of time, although you're missing some of the fun of this exercise if you do that.
- *
- * From the 32 bit RNG output, discover the seed.
- */
+namespace Cryptopals\Set3\Challenge22;
 
-require_once '21-implement-the-mt19937-mersenne-twister-rng.php';
+use Cryptopals\Set3\Challenge21\Solution21;
 
-// Sorry, going for simulated passage of time.
+class Solution22 extends Solution21
+{
+    protected function execute(): bool
+    {
+        // Sorry, going for simulated passage of time.
 
-$now = time();
+        $now = time();
 
-$mt = new MT19937();
-$mt->init($now - mt_rand(80, 2000)); // randception
-$r = $mt->int32();
+        $this->init($now - mt_rand(80, 2000)); // randception
+        $r = $this->int32();
 
-print "Got first random number: $r\n";
-print "Time is " . date('H:i:s', $now) . "\n\n";
+        print "Random number from the past: $r\n";
+        print "Time is " . date('H:i:s', $now) . "\n\n";
 
-for ($i = 0; $i <= 2000; $i++) {
-    $mt->init($now - $i); // randception
-    if ($mt->int32() === $r) {
-        print "RNG was seeded at: " . date('H:i:s', $now - $i) . "\n\n";
+        for ($i = 0; $i <= 2000; $i++) {
+            $this->init($now - $i);
+            if ($this->int32() === $r) {
+                print "RNG was seeded at: " . date('H:i:s', $now - $i) . "\n";
+                return true;
+            }
+        }
+
+        return false;
     }
 }
